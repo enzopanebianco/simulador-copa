2d997a177134b13ab4ff76e69222cfed26d5b201
Enzo Panebianco <enzo.romano@bcrservicos.com.br>
2022-10-04 09:09:20 -0300

    refatorado funcao de simular mata mata para uma única

--- Simulacao.js
+++ Simulacao.js
@@ -2,14 +2,14 @@ import { grupos } from "./constantes/Grupos.js";
 import { Partida } from "./Partida.js";
 
 const tabelaOitavas = {
-    'A1': 'B2',
-    'B1': 'A2',
-    'C1': 'D2',
-    'D1': 'C2',
-    'E1': 'F2',
-    'F1': 'E2',
-    'G1': 'H2',
-    'H1': 'G2'
+    '0': '3',
+    '2': '1',
+    '4': '7',
+    '6': '5',
+    '8': '11',
+    '10': '9',
+    '12': '15',
+    '14': '13'
 }
 
 const tabelaQuartas = {
@@ -24,6 +24,9 @@ const tabelaSemis = {
     '1':'3'
 }
 
+const tabelaFinal = {
+    '0':'1'
+}
 export class Simulacao {
     simularGrupos() {
         const classificados = []
@@ -36,59 +39,23 @@ export class Simulacao {
         console.table(classificados.flat())
         return classificados.flat()
     }
-    simularOitavas() {
-        const classificados = this.simularGrupos();
 
-        // pegando so os primeiros colocados para nao duplicar os jogos
-        const classificadosQuartas = classificados
-            .filter(classificado => classificado.posicaoGrupo.includes('1'))
-            .map(classificado => {
-                const visitante = classificados.find(selecao =>
-                    selecao.posicaoGrupo === tabelaOitavas[classificado.grupo + '1']
-                )
-                const partida = new Partida(classificado, visitante)
+    simularCopa(){
+        let classificados = this.simularGrupos();
+
+        const tabelas = [tabelaOitavas,tabelaQuartas,tabelaSemis,tabelaFinal]
+
+        tabelas.forEach(tabela=>{
+            classificados =  Object.entries(tabela).map(jogo=>{
+
+                const partida = new Partida(classificados[jogo[0]],classificados[jogo[1]])
 
                 const vencedor = partida.jogarMataMata()
 
                 return vencedor
             })
-        console.table(classificadosQuartas)
-        return classificadosQuartas
-    }
-    simularQuartas(){
-        const classificados = this.simularOitavas()
-        const classificadosSemis = []
-        Object.entries(tabelaQuartas).forEach(jogos=>{
-            const partida = new Partida(classificados[jogos[0]],classificados[jogos[1]])
-
-            const vencedor =  partida.jogarMataMata()
-            classificadosSemis.push(vencedor)
+            console.table(classificados)
         })
-        console.table(classificadosSemis)
-
-        return classificadosSemis
-    }
-    simularSemis(){
-        const classificados = this.simularQuartas()
-        const classificadosFinal = []
-        Object.entries(tabelaSemis).forEach(jogos=>{
-          
-            const partida = new Partida(classificados[jogos[0]],classificados[jogos[1]])
-
-            const vencedor =  partida.jogarMataMata()
-            classificadosFinal.push(vencedor)
-        })
-        console.table(classificadosFinal)
-        return classificadosFinal
     }
 
-    simularFinal(){
-        const finalistas = this.simularSemis()
-
-        const partida = new Partida(finalistas[0],finalistas[1])
-
-        const vencedor = partida.jogarMataMata()
-
-        console.table(vencedor)
-    }
 }
